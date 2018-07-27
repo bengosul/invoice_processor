2def817b10ed48a93dca6c89f31afc987b511252
begosul container <begosul@github.com>
2018-07-27 01:52:46 +0000

    embed 2015 uigrid version

--- config_angular/configs.php
+++ config_angular/configs.php
@@ -1,4 +1,8 @@
 <?php
+
+
+https://raw.githubusercontent.com/angular-ui/ui-grid.info/gh-pages/release/ui-grid-stable.min.js
+
 require_once '../functions/general_functions.php';
 session_start();
 validate_session('Invalid session');
@@ -12,7 +16,8 @@ validate_session('Invalid session');
     <script src="http://ui-grid.info/docs/grunt-scripts/csv.js"></script>
 <!--    <script src="http://ui-grid.info/docs/grunt-scripts/pdfmake.js"></script> -->
     <script src="http://ui-grid.info/docs/grunt-scripts/vfs_fonts.js"></script>
-    <script src="http://ui-grid.info/release/ui-grid-unstable.js"></script>
+<!--    <script src="https://rawgit.com/angular-ui/ui-grid.info/gh-pages/release/ui-grid-stable.min.js"></script> -->
+    <script src="ui-grid-stable.min.js"></script>
     <link rel="stylesheet" href="http://ui-grid.info/release/ui-grid-unstable.css" type="text/css">
 	<link rel="stylesheet" type="text/css" href="../styles/config.css">
 
